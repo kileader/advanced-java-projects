c4cbe307bff3a45bec0d7578d7bfff5c71c9b18b
Kevin Leader <kleader@madisoncollege.edu>
2020-02-12 14:23:11 -0600

    Fine tuning my project.

--- src/java112/analyzer/TokenAnalyzer.java
+++ src/java112/analyzer/TokenAnalyzer.java
@@ -1,10 +1,20 @@
 package java112.analyzer;
-
 /**
  * This interface is for any class that performs an analysis.
  * @author Kevin Leader
  */
 public interface TokenAnalyzer {
+    /**
+     * This is the general structure of the methods that will receive
+     * and get info from individual tokens.
+     * @param token singular word
+     */
     void processToken(String token);
+    /**
+     * This is the general structure of the methods that will generate the
+     * output required for the project.
+     * @param inputFilePath  the location of the file being read
+     * @param outputFilePath the location of the appropriate output file
+     */
     void generateOutputFile(String inputFilePath, String outputFilePath);
 }
