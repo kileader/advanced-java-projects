e3596d75f8da50be0258fe492726c1ab01f09641
Kevin Leader <kleader@madisoncollege.edu>
2020-03-02 13:21:54 -0600

    Added PropertiesLoader.java to java112.utilities

---
+++ src/java112/utilities/PropertiesLoader.java
@@ -0,0 +1,25 @@
+package java112.utilities;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author Eric Knapp
+ *
+ */
+public interface PropertiesLoader{
+
+    default Properties loadProperties(String propertiesFilePath) throws Exception {
+        Properties properties = new Properties();
+        try {
+            properties.load(this.getClass().getResourceAsStream(propertiesFilePath));
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            throw ioException;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            throw exception;
+        }
+        return properties;
+    }
+}
