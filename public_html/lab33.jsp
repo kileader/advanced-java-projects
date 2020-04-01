85e5a8a59e20f99113a6ac9c5574c6f514a91a24
Kevin Leader <kleader@madisoncollege.edu>
2020-04-01 14:14:02 -0500

    Getting through lab 3.

---
+++ public_html/lab33.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta name="generator" content="HTML Tidy, see www.w3.org" />
+
+    <title>Unit 3 Lab 3</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ src/java112/project3/Lab33Servlet.java
@@ -0,0 +1,35 @@
+package java112.project3;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+/**
+ * This is the second servlet for Project 3.
+ * @author Kevin Leader
+ */
+@WebServlet(
+        name = "Lab33Servlet",
+        urlPatterns = { "/Lab33Servlet" }
+)
+public class Lab33Servlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Map map = new HashMap();
+
+        map.put("number", 1);
+        map.put("text", "This is some sample text.");
+        map.put("html", "<h2>This is an h2</h2>");
+        map.put("aDate", new Date());
+
+        request.setAttribute("myMap", map);
+
+        String url = "/lab33.jsp";
+
+        RequestDispatcher  dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(request, response);
+    }
+}
