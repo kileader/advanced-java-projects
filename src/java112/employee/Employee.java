bd6e66ac41150b48faad5850b12070d694caa024
Kevin Leader <kleader@madisoncollege.edu>
2020-04-24 18:11:12 -0500

    Made a few classes for the project

---
+++ src/java112/employee/Employee.java
@@ -0,0 +1,74 @@
+package java112.employee;
+
+import java.util.*;
+
+public class Employee {
+
+    private String employeeID;
+    private String firstName;
+    private String lastName;
+    private String socialSecurity;
+    private String department;
+    private String room;
+    private String phone;
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSocialSecurity() {
+        return socialSecurity;
+    }
+
+    public void setSocialSecurity(String socialSecurity) {
+        this.socialSecurity = socialSecurity;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+//    public void toString() {
+//
+//    }
+}
---
+++ src/java112/employee/Search.java
@@ -0,0 +1,52 @@
+package java112.employee;
+
+import java.util.*;
+
+public class Search {
+
+    private String searchType;
+    private String searchTerm;
+    private List<String> results;
+    private boolean foundEmployees;
+
+    public Search() {
+
+    }
+
+    public String getSearchType() {
+        return searchType;
+    }
+
+    public void setSearchType(String searchType) {
+        this.searchType = searchType;
+    }
+
+    public String getSearchTerm() {
+        return searchTerm;
+    }
+
+    public void setSearchTerm(String searchTerm) {
+        this.searchTerm = searchTerm;
+    }
+
+    public List<String> getResults() {
+        return results;
+    }
+
+    public void setResults(List<String> results) {
+        this.results = results;
+    }
+
+    public boolean isFoundEmployees() {
+        return foundEmployees;
+    }
+
+    public void setFoundEmployees(boolean foundEmployees) {
+        this.foundEmployees = foundEmployees;
+    }
+
+    public void addFoundEmployee(Employee employee) {
+
+    }
+
+}
