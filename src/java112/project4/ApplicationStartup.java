25c8329c3b1319240b8739a4fd6162ef88b98fa9
Kevin Leader <kleader@madisoncollege.edu>
2020-05-10 14:51:51 -0500

    Finished ApplicationStartup servlet

--- src/java112/project4/ApplicationStartup.java
+++ src/java112/project4/ApplicationStartup.java
@@ -6,6 +6,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java112.utilities.*;
+import java112.employee.*;
 
 /**
  * This is a servlet that performs some initialization for the application.
@@ -21,12 +22,25 @@ public class ApplicationStartup extends HttpServlet
 
     private Properties properties;
 
+    /**
+     * This method sets the project 4 properties and an EmployeeDirectory object
+     * as ServletContext attributes.
+     * @throws ServletException
+     */
     public void init() throws ServletException {
+
         try {
             properties = loadProperties("/project4.properties");
         } catch (Exception exception) {
             exception.printStackTrace();
         }
+
+        ServletContext context = getServletContext();
+        context.setAttribute("project4Properties", properties);
+
+        EmployeeDirectory empDir = new EmployeeDirectory(properties);
+        context.setAttribute("employeeDirectory", empDir);
+
     }
 
 }
