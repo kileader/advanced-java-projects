92516c168e6d61a36ca6bce416dcd9cb9fea6027
Kevin Leader <kleader@madisoncollege.edu>
2020-05-10 17:32:21 -0500

    Working on EmployeeDirectory, specifically the adds and searches.

--- src/java112/employee/EmployeeDirectory.java
+++ src/java112/employee/EmployeeDirectory.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java112.utilities.*;
 
 /**
- * This class is for
+ * This class is for performing searches and adds to the employees table
  */
 public class EmployeeDirectory {
 
@@ -48,10 +48,10 @@ public class EmployeeDirectory {
     }
 
     /**
-     *
-     * @param args
+     * Adds a new record to the employee table in the database
+     * @param fields  Strings representing the fields of an employee record.
      */
-    public void addEmployee(String[] args) {
+    public void addEmployee(String[] fields) {
 
         Connection connection = establishConnection();
 
@@ -60,8 +60,9 @@ public class EmployeeDirectory {
             Statement statement = connection.createStatement();
 
             String insertString = "INSERT INTO employees VALUES (0, '" +
-                    args[0] + "', '" + args[1] + "', '" + args[2] + "', '" +
-                    args[3] + "', '" + args[4] + "', '" + args[5] + "')";
+                    fields[0] + "', '" + fields[1] + "', '" + fields[2] +
+                    "', '" + fields[3] + "', '" + fields[4] + "', '" +
+                    fields[5] + "')";
 
             System.out.println("insertString: " + insertString);
 
@@ -72,27 +73,87 @@ public class EmployeeDirectory {
         }
     }
 
-//    public Search searchEmployees(String searchTerm, String searchType) {
-//
-//    }
+    /**
+     * Performs a search on the employees table, depending on the type and term
+     * @param searchType    Describes the search type: ID, Last Name,
+     *                      or First Name
+     * @param searchTerm    Describes the actual ID, last name,
+     *                      or first name being searched
+     * @return              a Search object with the type and term set
+     */
+    public Search searchEmployees(String searchType, String searchTerm) {
 
-//    private void searchViaID(Search search) {
-//
-//        Connection connection = establishConnection();
-//
-//        try {
-//
-//            Statement statement = connection.createStatement();
-//
-//            String queryString = "SELECT * FROM employees WHERE employeeID = '"
-//                    + search.searchTerm;
-//
-//            System.out.println("insertString: " + insertString);
-//
-//            int rowsAffected = statement.executeUpdate(insertString);
-//
-//        } catch (SQLException sqlException) {
-//            sqlException.printStackTrace();
-//        }
-//    }
+        Search search = new Search();
+
+        search.setSearchType(searchType);
+        search.setSearchTerm(searchTerm);
+
+        if (searchType == "id") {
+            searchByID(search);
+        } else if (searchType == "lastName") {
+            searchByLastName(search);
+        } else if (searchType == "firstName") {
+            searchByFirstName(search);
+        }
+    }
+
+    private void searchByID(Search search) {
+
+        Connection connection = establishConnection();
+
+        try {
+
+            Statement statement = connection.createStatement();
+
+            String queryString = "SELECT * FROM employees WHERE employeeID = '"
+                    + search.searchTerm;
+
+            System.out.println("insertString: " + insertString);
+
+            int rowsAffected = statement.executeUpdate(insertString);
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+
+    private void searchByLastName(Search search) {
+
+        Connection connection = establishConnection();
+
+        try {
+
+            Statement statement = connection.createStatement();
+
+            String queryString = "SELECT * FROM employees WHERE employeeID = '"
+                    + search.searchTerm;
+
+            System.out.println("insertString: " + insertString);
+
+            int rowsAffected = statement.executeUpdate(insertString);
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+
+    private void searchByFirstName(Search search) {
+
+        Connection connection = establishConnection();
+
+        try {
+
+            Statement statement = connection.createStatement();
+
+            String queryString = "SELECT * FROM employees WHERE employeeID = '"
+                    + search.searchTerm;
+
+            System.out.println("insertString: " + insertString);
+
+            int rowsAffected = statement.executeUpdate(insertString);
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
 }
